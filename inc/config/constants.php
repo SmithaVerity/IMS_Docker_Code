063f9d7620d1c1ada7c82ae0e65438ab5dd7eb04
SmithaVerity <smitha@theveritycorp.com>
2022-09-26 01:57:14 +0530

    Added code

---
+++ inc/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+	// Root url for the site
+	define('ROOT_URL', 'http://34.208.162.213/IMS');
+	
+	
+	// Database parameters
+	// Data source name
+	define('DSN', 'mysql:host=localhost;dbname=shop_inventory');
+	
+	// Hostname
+	define('DB_HOST', 'localhost');
+	
+	// DB user
+	define('DB_USER', 'ubuntu');
+	
+	// DB password
+	define('DB_PASSWORD', 'Ubuntu@123');
+	
+	// DB name
+	define('DB_NAME', 'shop_inventory');
+?>
